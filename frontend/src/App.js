e5cc9a815baecae3fb15b68f3fccbf4f39ff4268
GuiBispo24 <guibispo220206@gmail.com>
2025-11-01 15:11:24 -0300

    Front-End update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import LoginUser from './components/User/LoginUser';
-import AddUser from './components/User/AddUser';
-import Feed from './components/User/Feed';
-import NewDenuncia from './components/User/NewDenuncia';
 
 function App() {
   return (
@@ -11,10 +7,6 @@ function App() {
       <header className="App-header">
         <BrowserRouter>
           <Routes>
-            <Route path='/login' element={<LoginUser/>}></Route>
-            <Route path='/cadastro' element={<AddUser/>}></Route>
-            <Route path='/feed' element={<Feed/>}></Route>
-            <Route path='/newDenuncia' element={<NewDenuncia/>}></Route>
           </Routes>
         </BrowserRouter>     
       </header>
