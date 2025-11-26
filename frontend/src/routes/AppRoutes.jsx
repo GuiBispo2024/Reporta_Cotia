d7cff7398032adf0255f9a02c0940626758399fb
GuiBispo2024 <guibispo220206@gmail.com>
2025-11-26 10:41:43 -0300

    routes update

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,8 +4,10 @@ import Cadastro from "../pages/Cadastro.jsx";
 import Home from "../pages/Home.jsx";
 import Moderacao from "../pages/Moderacao.jsx";
 import MinhasDenuncias from "../pages/MinhasDenuncias.jsx";
+import EditarDenuncia from "../pages/EditarDenuncia.jsx";
 import NovaDenuncia from "../pages/NovaDenuncia.jsx";
 import Perfil from "../pages/Perfil.jsx";
+import ListaDeUsuários from "../pages/ListaDeUsuários.jsx";
 import PrivateRoute from "./PrivateRoute.jsx";
 
 function AppRoutes() {
@@ -17,7 +19,9 @@ function AppRoutes() {
                 <Route path="/cadastro" element={<Cadastro/>}/>
                 <Route path="/moderacao" element={<PrivateRoute><Moderacao/></PrivateRoute>}/>
                 <Route path="/minhas-denuncias" element={<PrivateRoute><MinhasDenuncias/></PrivateRoute>}/>
+                <Route path="/editar-denuncia/:id" element={<PrivateRoute><EditarDenuncia/></PrivateRoute>}/>
                 <Route path="/nova-denuncia" element={<PrivateRoute><NovaDenuncia/></PrivateRoute>}/>
+                <Route path="/lista-de-usuarios" element={<PrivateRoute><ListaDeUsuários/></PrivateRoute>}/>
                 <Route path="/perfil" element={<PrivateRoute><Perfil/></PrivateRoute>}/>
             </Routes>
         </Router>
