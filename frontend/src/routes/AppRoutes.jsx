04b3951c956d37b0d6a052858cb3eb4b8dcdda3b
GuiBsipo2024 <guibispo220206@gmail.com>
2026-09-05 19:30:23 -0300

    feat(ui): atualizar acessibilidade filtros e identidade visual

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -9,8 +9,15 @@ import NovaDenuncia from "../pages/NovaDenuncia.jsx";
 import Perfil from "../pages/Perfil.jsx";
 import EditarPerfil from "../pages/EditarPerfil.jsx";
 import DetalheDenuncia from "../pages/DetalheDenuncia.jsx";
+import CurtidasDenuncia from "../pages/CurtidasDenuncia.jsx";
+import CompartilhamentosDenuncia from "../pages/CompartilhamentosDenuncia.jsx";
 import ListaDeUsuários from "../pages/ListaDeUsuários.jsx";
 import PrivateRoute from "./PrivateRoute.jsx";
+import PerfilPublico from '../pages/PerfilPublico.jsx';
+import HistoricoDenuncia from '../pages/HistoricoDenuncia.jsx';
+import EsqueciSenha from '../pages/EsqueciSenha.jsx';
+import RedefinirSenha from '../pages/RedefinirSenha.jsx';
+import HistoricoRedefinicoesSenha from '../pages/HistoricoRedefinicoesSenha.jsx';
 
 function AppRoutes() {
     return(
@@ -18,9 +25,16 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
+                <Route path="/esqueci-senha" element={<EsqueciSenha/>}/>
+                <Route path="/redefinir-senha" element={<RedefinirSenha/>}/>
                 <Route path="/denuncia/:id" element={<DetalheDenuncia/>}/>
+                <Route path="/denuncia/:id/curtidas" element={<CurtidasDenuncia/>}/>
+                <Route path="/denuncia/:id/compartilhamentos" element={<CompartilhamentosDenuncia/>}/>
                 <Route path="/cadastro" element={<Cadastro/>}/>
+                <Route path="/usuarios/:id" element={<PerfilPublico/>}/>
                 <Route path="/moderacao" element={<PrivateRoute><Moderacao/></PrivateRoute>}/>
+                <Route path="/moderacao/denuncia/:id/historico" element={<PrivateRoute><HistoricoDenuncia/></PrivateRoute>}/>
+                <Route path="/moderacao/redefinicoes-senha" element={<PrivateRoute><HistoricoRedefinicoesSenha/></PrivateRoute>}/>
                 <Route path="/minhas-denuncias" element={<PrivateRoute><MinhasDenuncias/></PrivateRoute>}/>
                 <Route path="/editar-denuncia/:id" element={<PrivateRoute><EditarDenuncia/></PrivateRoute>}/>
                 <Route path="/nova-denuncia" element={<PrivateRoute><NovaDenuncia/></PrivateRoute>}/>
